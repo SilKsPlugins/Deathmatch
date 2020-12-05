192b4b3b89f14f043ae7580dd5b18a191f8d1a33
Stephen White <iamsilk3495@gmail.com>
2020-12-05 14:43:18 -0400

    Added revoking of grace if attacking

--- Deathmatch.Core/Grace/GraceManager.cs
+++ Deathmatch.Core/Grace/GraceManager.cs
@@ -1,20 +1,40 @@
 ﻿using Deathmatch.API.Players;
+using HarmonyLib;
 using Microsoft.Extensions.DependencyInjection;
+using OpenMod.API;
+using OpenMod.API.Eventing;
 using OpenMod.API.Ioc;
-using OpenMod.API.Prioritization;
+using OpenMod.Unturned.Players.Life.Events;
+using SDG.Unturned;
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using Priority = OpenMod.API.Prioritization.Priority;
 
 namespace Deathmatch.Core.Grace
 {
     [ServiceImplementation(Lifetime = ServiceLifetime.Singleton, Priority = Priority.Lowest)]
-    public class GraceManager : IGraceManager
+    public class GraceManager : IGraceManager, IDisposable
     {
+        private readonly IGamePlayerManager _playerManager;
         private readonly Dictionary<IGamePlayer, float> _gracedPlayers;
 
-        public GraceManager()
+        public GraceManager(IGamePlayerManager playerManager,
+            IEventBus eventBus,
+            IRuntime runtime)
         {
+            _playerManager = playerManager;
             _gracedPlayers = new Dictionary<IGamePlayer, float>();
+
+            OnEquipmentInput += Events_OnEquipmentInput;
+
+            eventBus.Subscribe(runtime, (EventCallback<UnturnedPlayerDamagingEvent>)OnPlayerDamaging);
+        }
+
+        public void Dispose()
+        {
+            OnEquipmentInput -= Events_OnEquipmentInput;
         }
 
         public bool WithinGracePeriod(IGamePlayer player)
@@ -46,6 +66,38 @@ namespace Deathmatch.Core.Grace
         public void RevokeGracePeriod(IGamePlayer player)
         {
             _gracedPlayers.Remove(player);
+        }
+
+        private void Events_OnEquipmentInput(Player nativePlayer, bool inputPrimary, bool inputSecondary)
+        {
+            var player = _playerManager.GetPlayer(nativePlayer);
+
+            if (WithinGracePeriod(player))
+                RevokeGracePeriod(player);
+        }
+
+        public Task OnPlayerDamaging(IServiceProvider serviceProvider, object sender, UnturnedPlayerDamagingEvent @event)
+        {
+            var player = _playerManager.GetPlayer(@event.Player);
+
+            if (WithinGracePeriod(player))
+                @event.IsCancelled = true;
+
+            return Task.CompletedTask;
+        }
+
+        private delegate void EquipmentInput(Player player, bool inputPrimary, bool inputSecondary);
+        private static event EquipmentInput OnEquipmentInput;
+
+        [HarmonyPatch]
+        private class Patches
+        {
+            [HarmonyPatch(typeof(PlayerEquipment), "simulate")]
+            [HarmonyPrefix]
+            private static void Simulate(PlayerEquipment __instance, bool inputPrimary, bool inputSecondary)
+            {
+                OnEquipmentInput?.Invoke(__instance.player, inputPrimary, inputSecondary);
+            }
         }
     }
 }
