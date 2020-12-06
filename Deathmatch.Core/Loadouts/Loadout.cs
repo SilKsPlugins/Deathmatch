6d8f7fe1d82eec48f21cb87b991fc4dceefa9b4c
Stephen White <iamsilk3495@gmail.com>
2020-12-06 17:51:15 -0400

    Added Loadout.FromPlayer method

--- Deathmatch.Core/Loadouts/Loadout.cs
+++ Deathmatch.Core/Loadouts/Loadout.cs
@@ -1,8 +1,9 @@
 ﻿using Deathmatch.API.Loadouts;
 using Deathmatch.API.Players;
-using Deathmatch.Core.Items;
+using SDG.Unturned;
 using System;
 using System.Collections.Generic;
+using Item = Deathmatch.Core.Items.Item;
 
 namespace Deathmatch.Core.Loadouts
 {
@@ -42,6 +43,37 @@ namespace Deathmatch.Core.Loadouts
             {
                 item.GiveToPlayer(player);
             }
+        }
+
+        public static Loadout FromPlayer(IGamePlayer player)
+        {
+            var loadout = new Loadout();
+
+            var c = player.Clothing;
+
+            if (c.backpack != 0) loadout.Items.Add(new Item(c.backpack, 1, c.backpackQuality, c.backpackState));
+            if (c.glasses != 0) loadout.Items.Add(new Item(c.glasses, 1, c.glassesQuality, c.glassesState));
+            if (c.hat != 0) loadout.Items.Add(new Item(c.hat, 1, c.hatQuality, c.hatState));
+            if (c.mask != 0) loadout.Items.Add(new Item(c.mask, 1, c.maskQuality, c.maskState));
+            if (c.pants != 0) loadout.Items.Add(new Item(c.pants, 1, c.pantsQuality, c.pantsState));
+            if (c.shirt != 0) loadout.Items.Add(new Item(c.shirt, 1, c.shirtQuality, c.shirtState));
+            if (c.vest != 0) loadout.Items.Add(new Item(c.vest, 1, c.vestQuality, c.vestState));
+
+            for (byte page = 0; page < PlayerInventory.PAGES - 2; page++)
+            {
+                var count = player.Inventory.getItemCount(page);
+
+                for (byte i = 0; i < count; i++)
+                {
+                    var jar = player.Inventory.getItem(page, i);
+
+                    if (jar?.item == null) continue;
+
+                    loadout.Items.Add(Item.FromUnturnedItem(jar.item));
+                }
+            }
+
+            return loadout;
         }
     }
 }
