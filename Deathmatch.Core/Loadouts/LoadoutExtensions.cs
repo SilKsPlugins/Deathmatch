2a9f1611cf30d8f40a07a7d9d7ebd78e5b9f5024
Stephen White <iamsilk3495@gmail.com>
2020-12-06 20:38:05 -0400

    Added player-based random loadout extension

---
+++ Deathmatch.Core/Loadouts/LoadoutExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Deathmatch.API.Loadouts;
+using Deathmatch.API.Players;
+using Deathmatch.Core.Helpers;
+using OpenMod.API.Permissions;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Deathmatch.Core.Loadouts
+{
+    public static class LoadoutExtensions
+    {
+        public static async Task<ILoadout> GetRandomLoadout(this ILoadoutManager loadoutManager, string categoryTitle,
+            IGamePlayer player, IPermissionChecker permissionChecker = null)
+        {
+            var category = loadoutManager.GetCategory(categoryTitle);
+
+            if (category == null) return null;
+
+            if (permissionChecker == null)
+                return category.GetLoadouts().RandomElement();
+
+            foreach (var randomLoadout in category.GetLoadouts().ToList().Shuffle())
+            {
+                if (await permissionChecker.CheckPermissionAsync(player.User, randomLoadout.Permission) ==
+                    PermissionGrantResult.Grant)
+                    return randomLoadout;
+            }
+
+            return null;
+        }
+    }
+}
