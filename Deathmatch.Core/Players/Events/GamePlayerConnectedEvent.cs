76b61c08764f11c0c0b6cc52c894fd940e7f8399
Stephen White <iamsilk3495@gmail.com>
2020-12-07 19:08:01 -0400

    Fixed game player connecting/disconnect improper inheritance

--- Deathmatch.Core/Players/Events/GamePlayerConnectedEvent.cs
+++ Deathmatch.Core/Players/Events/GamePlayerConnectedEvent.cs
@@ -1,8 +1,9 @@
 ﻿using Deathmatch.API.Players;
+using Deathmatch.API.Players.Events;
 
 namespace Deathmatch.Core.Players.Events
 {
-    public class GamePlayerConnectedEvent : GamePlayerEvent
+    public class GamePlayerConnectedEvent : GamePlayerEvent, IGamePlayerConnectedEvent
     {
         public GamePlayerConnectedEvent(IGamePlayer player) : base(player)
         {
