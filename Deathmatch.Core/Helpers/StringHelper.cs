4d30f1d10b3e04481944e62d53a6f42088b3b4a8
Stephen White <iamsilk3495@gmail.com>
2021-07-07 09:43:25 -0300

    Added FindBestMatch to string helper and removed Distance

--- Deathmatch.Core/Helpers/StringHelper.cs
+++ Deathmatch.Core/Helpers/StringHelper.cs
@@ -1,52 +1,19 @@
-﻿using System;
+﻿using MoreLinq.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Deathmatch.Core.Helpers
 {
     public static class StringHelper
     {
-        // Levenshtein distance between two strings
-        public static int Distance(string s, string t, bool ignoreCase = true)
+        public static T FindBestMatch<T>(this IEnumerable<T> enumerable, Func<T, string> termSelector, string searchString)
         {
-            if (ignoreCase)
-            {
-                s = s.ToLower();
-                t = t.ToLower();
-            }
-
-            int n = s.Length;
-            int m = t.Length;
-            int[,] d = new int[n + 1, m + 1];
-
-            if (n == 0)
-            {
-                return m;
-            }
-
-            if (m == 0)
-            {
-                return n;
-            }
-
-            for (int i = 0; i <= n; d[i, 0] = i++)
-            {
-            }
-
-            for (int j = 0; j <= m; d[0, j] = j++)
-            {
-            }
-
-            for (int i = 1; i <= n; i++)
-            {
-                for (int j = 1; j <= m; j++)
-                {
-                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
-
-                    d[i, j] = Math.Min(
-                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
-                        d[i - 1, j - 1] + cost);
-                }
-            }
-            return d[n, m];
+            return enumerable.Where(x =>
+                    termSelector(x).IndexOf(searchString, StringComparison.OrdinalIgnoreCase) >= 0)
+                .MinBy(asset =>
+                    OpenMod.Core.Helpers.StringHelper.LevenshteinDistance(searchString, termSelector(asset)))
+                .FirstOrDefault();
         }
     }
 }
