cdab72e8fcc6cf3ffb8276ab53e653d5052c6463
Stephen White <iamsilk3495@gmail.com>
2021-07-07 14:22:31 -0300

    Update group preservation

--- Deathmatch.Core/Preservation/Groups/PreservedGroup.cs
+++ Deathmatch.Core/Preservation/Groups/PreservedGroup.cs
@@ -1,6 +1,5 @@
 ﻿using SDG.Unturned;
 using Steamworks;
-using System.Reflection;
 
 namespace Deathmatch.Core.Preservation.Groups
 {
@@ -8,33 +7,18 @@ namespace Deathmatch.Core.Preservation.Groups
     {
         private readonly CSteamID _groupId;
         private readonly EPlayerGroupRank _groupRank;
-        private readonly bool _inMainGroup;
-
-        private static readonly FieldInfo InMainGroup =
-            typeof(PlayerQuests).GetField("inMainGroup", BindingFlags.Instance | BindingFlags.NonPublic);
 
         public PreservedGroup(PlayerQuests quests)
         {
             _groupId = quests.groupID;
             _groupRank = quests.groupRank;
-            _inMainGroup = (bool)InMainGroup.GetValue(quests);
 
-            quests.channel.send("tellSetGroup", ESteamCall.ALL, ESteamPacket.UPDATE_RELIABLE_BUFFER, new object[]
-            {
-                CSteamID.Nil,
-                (byte)0
-            });
-            InMainGroup.SetValue(quests, false);
+            quests.leaveGroup(true);
         }
 
         public void Restore(PlayerQuests quests)
         {
-            quests.channel.send("tellSetGroup", ESteamCall.ALL, ESteamPacket.UPDATE_RELIABLE_BUFFER, new object[]
-            {
-                _groupId,
-                (byte)_groupRank
-            });
-            InMainGroup.SetValue(quests, _inMainGroup);
+            quests.ServerAssignToGroup(_groupId, _groupRank, true);
         }
     }
 }
